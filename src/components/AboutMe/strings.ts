a1ae52c58c9d5e2e6878029426b27fe22935f27e
Yesica Romina Lato <108998939+Yerola@users.noreply.github.com>
2023-08-29 15:53:04 -0300

    Update strings.ts

--- src/components/AboutMe/strings.ts
+++ src/components/AboutMe/strings.ts
@@ -3,12 +3,12 @@ import type { AboutMeStrings } from '../../types/strings';
 export const strings: AboutMeStrings = {
   en: {
     aboutMe: 'About Me',
-    description: "Fullstack developer, Front-end lover. Studying a Bachelor's Degree in Systems Engineering. I attended a bootcamp with more than 900 hours of intensive learning that prepared me to work as a web developer, creating projects that were both real and complex. Furthermore, the bootcamp greatly improved my interpersonal skills and gave me the opportunity of teaching to beginner students.",
+    description: "Fullstack developer, Front-end lover. Studying a Bachelor's Degree in Software Engineering. I attended a bootcamp with more than 900 hours of intensive learning that prepared me to work as a web developer, creating projects that were both real and complex. Furthermore, the bootcamp greatly improved my interpersonal skills and gave me the opportunity of teaching to beginner students.",
     seeProjects: 'See my projects',
   },
   es: {
     aboutMe: 'Sobre mí',
-    description: 'Desarrolladora fullstack, amante del Front-end. Estudiando Licenciatura en Ingeniería de Sistemas. Realicé un bootcamp de más de 900 hs. de estudio intensivo que me preparó para desempeñarme como desarrolladora web, ideando proyectos reales y complejos, además de potenciar mis habilidades interpersonales y haberme permitido ser mentora de estudiantes principiantes.',
+    description: 'Desarrolladora fullstack, amante del Front-end. Estudiando Licenciatura en Ingeniería de Software. Realicé un bootcamp de más de 900 hs. de estudio intensivo que me preparó para desempeñarme como desarrolladora web, ideando proyectos reales y complejos, además de potenciar mis habilidades interpersonales y haberme permitido ser mentora de estudiantes principiantes.',
     seeProjects: 'Ver mis proyectos',
   },
 };
